152afb7863dddc38375dd4512422947d8921448d
Gonzalo Sanchez <go.sanchezm@gmail.com>
2024-12-01 20:11:10 -0300

    feat: challenges 1 & 2

---
+++ README.md
@@ -0,0 +1,16 @@
+# Adventjs
+
+Repo para el completar los challenges del [adventjs 🎄](https://adventjs.dev/es)
+
+# Como usar el repo
+
+1. Instalar deps
+
+```
+npm i
+```
+
+2. Como ejecutar los archivos:
+
+- Usando vscode: **abrir ese archivo y apretar F5**. Esto te permite agregar puntos de interrupción.
+- Alternativamente puedes ejecutar directamente usando ts-node con `npx ts-node src/02-EnmarcandoNombres/index.ts`
---
+++ src/02-EnmarcandoNombres/index.ts
@@ -0,0 +1,75 @@
+/*
+  Santa Claus 🎅 quiere enmarcar los nombres de los niños buenos para decorar su taller 🖼️, 
+  pero el marco debe cumplir unas reglas específicas. Tu tarea es ayudar a los elfos a 
+  generar este marco mágico.
+
+  Reglas:
+  Dado un array de nombres, debes crear un marco rectangular que los contenga a todos.
+  Cada nombre debe estar en una línea, alineado a la izquierda.
+  El marco está construido con * y tiene un borde de una línea de ancho.
+  La anchura del marco se adapta automáticamente al nombre más largo más un margen de 1 espacio a cada lado.
+*/
+
+function createFrame(names: string[]): string {
+  // Code here
+  let maxNameLength = 0;
+  let rows = 0;
+  let cols = 0;
+
+  names.forEach((name) => {
+    rows++;
+    maxNameLength = Math.max(maxNameLength, name.length);
+  });
+
+  cols = maxNameLength + 4;
+
+  let str = "*".repeat(cols);
+  str += "\n";
+
+  names.forEach((name) => {
+    const nameStr = `* ${name}${" ".repeat(cols - name.length - 3)}*\n`;
+    str += nameStr;
+  });
+
+  str += "*".repeat(cols);
+
+  return str;
+}
+
+// tests
+console.log(createFrame(["midu", "madeval", "educalvolpz"]));
+
+/* Resultado esperado:
+ ***************
+ * midu        *
+ * madeval     *
+ * educalvolpz *
+ ***************
+ */
+
+console.log(createFrame(["midu"]));
+
+/* Resultado esperado:
+ ********
+ * midu *
+ ********
+ */
+
+console.log(createFrame(["a", "bb", "ccc"]));
+
+/* Resultado esperado:
+ *******
+ * a   *
+ * bb  *
+ * ccc *
+ *******
+ */
+
+console.log(createFrame(["a", "bb", "ccc", "dddd"]));
+
+// ***************
+// * midu          *
+// * madeval       *
+// * educalvolpz   *
+
+// * midu          *\n
